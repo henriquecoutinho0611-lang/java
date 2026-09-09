54a02aca5745444502f1d342a0a0d4eb3392f4d5
Henrique Coutinho <henriquecoutinho0611@gmail.com>
2026-09-09 08:59:16 -0300

    Atualizar o ProdutoResponse.java

--- produtos/src/main/java/com/example/produtos/DTO/Response/ProdutoResponse.java
+++ produtos/src/main/java/com/example/produtos/DTO/Response/ProdutoResponse.java
@@ -1,6 +1,6 @@
 package com.example.produtos.DTO.Response;
 
-public record ProdutoResponse(long id, String nome, String largura, String altura, String comprimento, String profundidade, String categoria, String descricao, String observacao) {
+public record ProdutoResponse(Long id, String nome, String largura, String altura, String comprimento, String profundidade, String categoria, String descricao, String observacao) {
 }
 
 
