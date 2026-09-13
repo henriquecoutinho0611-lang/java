aacb0ab294e81c0d3229a8ce02a33919b3d7f46b
Henrique Coutinho <henriquecoutinho0611@gmail.com>
2026-09-13 13:50:59 -0300

    Fix: Corrigir nome da classe (Controller), método (calcular), adicionar @RequestBody e corrigir nomes

---
+++ caculosTrabalista/src/main/java/com/example/caculosTrabalista/Controller.java
@@ -0,0 +1,27 @@
+package com.example.caculosTrabalista;
+
+import com.example.caculosTrabalista.entity.Empregado;
+import com.example.caculosTrabalista.service.EmpregadoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/calculos")
+public class Controller {
+
+    private EmpregadoService empregadoService;
+
+    public Controller(EmpregadoService empregadoService) {
+        this.empregadoService = empregadoService;
+    }
+
+    @PostMapping()
+    public BigDecimal calcular(@RequestBody Empregado empregado){
+        return empregadoService.salarioLiquido(empregado);
+    }
+
+}
